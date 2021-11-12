a5567acf63373d27dee78ec855bf008ea37e87ae
Shu <gmorales@PC861>
2021-11-12 16:21:04 -0700

    contact manager NOT bullet colision

--- src/ContactEventManager.cc
+++ src/ContactEventManager.cc
@@ -1,6 +1,7 @@
 #include "ContactEventManager.hh"
 #include "GameObject.hh"
 #include<iostream>
+#include<string>
 
 ContactEventManager::ContactEventManager()
 {
@@ -15,15 +16,19 @@ void ContactEventManager::BeginContact(b2Contact *contact)
   GameObject* actorA{(GameObject*)contact->GetFixtureA()->GetBody()->GetUserData().pointer};
   GameObject* actorB{(GameObject*)contact->GetFixtureB()->GetBody()->GetUserData().pointer};
 
-  if(actorA && actorB)
+  std::string a = actorA->GetTagName();
+  std::string b = actorB->GetTagName();
+
+  if(actorA && actorB )
   {
-    if(actorA->GetTagName() != "Player" && actorB->GetTagName() != "Bullet"){
+    if(!(a == "Player" && b == "Bullet")){
       std::cout << "c " << actorA->GetTagName() << ", " << actorB->GetTagName() << std::endl;
       std::cout << "collision: " << actorA->GetTagName() << ", " << actorB->GetTagName() << std::endl;
     }
+    
   }
 }
 void ContactEventManager::EndContact(b2Contact *contact)
 {
-
+  std::cout << "mmm patas" << std::endl;
 }
