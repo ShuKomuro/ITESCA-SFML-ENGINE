8dd1f94edf9d551ac64f9c0ad16e1972d41fcc45
LuisArias1998 <ariasmontoya_luis@hotmail.com>
2021-11-23 12:25:59 -0700

    Mapeado casi terminado

--- src/Game.cc
+++ src/Game.cc
@@ -20,6 +20,22 @@ Wall* wall1{};
 GameObject* wall2{};
 GameObject* wall3{};
 GameObject* wall4{};
+GameObject* wall5{};
+GameObject* wall6{};
+GameObject* wall7{};
+GameObject* wall8{};
+GameObject* wall9{};
+GameObject* wall10{};
+GameObject* wall11{};
+GameObject* wall12{};
+GameObject* wall13{};
+GameObject* wall14{};
+GameObject* wall15{};
+GameObject* wall16{};
+GameObject* wall17{};
+GameObject* wall18{};
+GameObject* wall19{};
+GameObject* wall20{};
 Enemy* enemy1{};
 DrawMap* walls{};
 DrawMap* floorT{};
@@ -64,21 +80,129 @@ Game::Game()
   new sf::Vector2f(600, 500), window, world, enemySpeed);
   enemy1->SetTagName("Enemy");
 
-  wall1 = new Wall("", 0, 0, 10.f, 600.f, playerScale, 
+  wall1 = new Wall("", 0, 0, 10.f, 2500.f, playerScale, 
   new sf::Vector2f(0, 0), b2BodyType::b2_staticBody, window, world, true);
   wall1->SetTagName("Wall");
 
-  wall2 = new Wall("", 0, 0, 10.f, 600.f, playerScale, 
+  wall2 = new Wall("", 0, 0, 10.f, 330.f, playerScale, 
   new sf::Vector2f(416, 0), b2BodyType::b2_staticBody, window, world, true);
   wall2->SetTagName("Wall");
 
   wall3 = new Wall("", 0, 0, 832.f, 10.f, playerScale, 
-  new sf::Vector2f(0, 0), b2BodyType::b2_staticBody, window, world, true);
+  new sf::Vector2f(0, 16), b2BodyType::b2_staticBody, window, world, true);
   wall3->SetTagName("Wall");
 
-  wall4 = new Wall("", 0, 0, 832.f, 10.f, playerScale, 
-  new sf::Vector2f(0, 300), b2BodyType::b2_staticBody, window, world, true);
+  wall4 = new Wall("", 0, 0, 200.f, 10.f, playerScale, 
+  new sf::Vector2f(0, 285), b2BodyType::b2_staticBody, window, world, true);
   wall4->SetTagName("Wall");
+
+    wall4 = new Wall("", 0, 0, 10.f, 70.f, playerScale, 
+    new sf::Vector2f(100, 285), b2BodyType::b2_staticBody, window, world, true);
+    wall4->SetTagName("Wall");
+
+    wall4 = new Wall("", 0, 0, 10.f, 70.f, playerScale, 
+    new sf::Vector2f(185, 285), b2BodyType::b2_staticBody, window, world, true);
+    wall4->SetTagName("Wall");
+
+    wall4 = new Wall("", 0, 0, 400.f, 10.f, playerScale, 
+  new sf::Vector2f(400, 285), b2BodyType::b2_staticBody, window, world, true);
+  wall4->SetTagName("Wall");
+
+  wall2 = new Wall("", 0, 0, 10.f, 130.f, playerScale, 
+  new sf::Vector2f(285, 100), b2BodyType::b2_staticBody, window, world, true);
+  wall2->SetTagName("Wall");
+
+  wall2 = new Wall("", 0, 0, 150.f, 10.f, playerScale, 
+  new sf::Vector2f(355, 145), b2BodyType::b2_staticBody, window, world, true);
+  wall2->SetTagName("Wall");
+
+  wall2 = new Wall("", 0, 0, 100.f, 10.f, playerScale, 
+  new sf::Vector2f(460, 185), b2BodyType::b2_staticBody, window, world, true);
+  wall2->SetTagName("Wall");
+
+  wall2 = new Wall("", 0, 0, 10.f, 180.f, playerScale, 
+  new sf::Vector2f(505, 90), b2BodyType::b2_staticBody, window, world, true);
+  wall2->SetTagName("Wall");
+
+  wall2 = new Wall("", 0, 0, 400.f, 10.f, playerScale, 
+  new sf::Vector2f(655, 25), b2BodyType::b2_staticBody, window, world, true);
+  wall2->SetTagName("Wall");
+
+  wall2 = new Wall("", 0, 0, 10.f, 1500.f, playerScale, 
+  new sf::Vector2f(830, 700), b2BodyType::b2_staticBody, window, world, true);
+  wall2->SetTagName("Wall");
+
+  wall2 = new Wall("", 0, 0, 200.f, 10.f, playerScale, 
+  new sf::Vector2f(830, 90), b2BodyType::b2_staticBody, window, world, true);
+  wall2->SetTagName("Wall");
+
+  wall2 = new Wall("", 0, 0, 200.f, 10.f, playerScale, 
+  new sf::Vector2f(830, 185), b2BodyType::b2_staticBody, window, world, true);
+  wall2->SetTagName("Wall");
+
+  wall2 = new Wall("", 0, 0, 10.f, 100.f, playerScale, 
+  new sf::Vector2f(740, 142), b2BodyType::b2_staticBody, window, world, true);
+  wall2->SetTagName("Wall");
+
+
+    wall2 = new Wall("", 0, 0, 200.f, 10.f, playerScale, 
+  new sf::Vector2f(890, 405), b2BodyType::b2_staticBody, window, world, true);
+  wall2->SetTagName("Wall");
+
+  wall2 = new Wall("", 0, 0, 200.f, 10.f, playerScale, 
+  new sf::Vector2f(890, 475), b2BodyType::b2_staticBody, window, world, true);
+  wall2->SetTagName("Wall");
+
+  wall2 = new Wall("", 0, 0, 10.f, 60.f, playerScale, 
+  new sf::Vector2f(800, 442), b2BodyType::b2_staticBody, window, world, true);
+  wall2->SetTagName("Wall");
+
+
+
+  //PAREDES HORIZONTALES 2 Y 3 
+  wall4 = new Wall("", 0, 0, 200.f, 10.f, playerScale, 
+  new sf::Vector2f(0, 310), b2BodyType::b2_staticBody, window, world, true);
+  wall4->SetTagName("Wall");
+
+  wall4 = new Wall("", 0, 0, 400.f, 10.f, playerScale, 
+  new sf::Vector2f(400, 310), b2BodyType::b2_staticBody, window, world, true);
+  wall4->SetTagName("Wall");
+
+   wall4 = new Wall("", 0, 0, 375.f, 10.f, playerScale, 
+  new sf::Vector2f(375, 570), b2BodyType::b2_staticBody, window, world, true);
+  wall4->SetTagName("Wall");
+
+  wall4 = new Wall("", 0, 0, 200.f, 10.f, playerScale, 
+  new sf::Vector2f(0, 570), b2BodyType::b2_staticBody, window, world, true);
+  wall4->SetTagName("Wall");
+
+  wall4 = new Wall("", 0, 0, 375.f, 10.f, playerScale, 
+  new sf::Vector2f(375, 595), b2BodyType::b2_staticBody, window, world, true);
+  wall4->SetTagName("Wall");
+
+  wall4 = new Wall("", 0, 0, 200.f, 10.f, playerScale, 
+  new sf::Vector2f(0, 595), b2BodyType::b2_staticBody, window, world, true);
+  wall4->SetTagName("Wall");
+
+  wall4 = new Wall("", 0, 0, 375.f, 10.f, playerScale, 
+  new sf::Vector2f(375, 850), b2BodyType::b2_staticBody, window, world, true);
+  wall4->SetTagName("Wall");
+
+  
+  /////
+
+  wall4 = new Wall("", 0, 0, 10.f, 160.f, playerScale, 
+  new sf::Vector2f(415, 360), b2BodyType::b2_staticBody, window, world, true);
+  wall4->SetTagName("Wall");
+
+  wall4 = new Wall("", 0, 0, 100.f, 10.f, playerScale, 
+  new sf::Vector2f(460, 440), b2BodyType::b2_staticBody, window, world, true);
+  wall4->SetTagName("Wall");
+
+    wall4 = new Wall("", 0, 0, 155.f, 10.f, playerScale, 
+  new sf::Vector2f(490, 540), b2BodyType::b2_staticBody, window, world, true);
+  wall4->SetTagName("Wall");
+
 
   //tileGroup = new TileGroup(window, 10, 10, "assets/tile.png", "assets/maps/map1.tg", 16, 16, 4.f);
 
